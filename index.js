29420dc22e594403d0b6bd47cbf201a89ea9a762
Arpit Vadariya <102517996+ArpitVadariya@users.noreply.github.com>
2023-06-07 18:36:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('ARPIT PATEL in Cloud'); //write a response to the client
+  res.write('ARPIT VADARIYA in Cloud'); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 80
